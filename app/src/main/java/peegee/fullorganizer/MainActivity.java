9af36879d4fef8be1a11716ab1425e7afdccc5c8
PeeGee92 <pierre_ghaly@yahoo.com>
2018-04-20 00:54:20 +0300

    Firebase progress

--- app/src/main/java/peegee/fullorganizer/MainActivity.java
+++ app/src/main/java/peegee/fullorganizer/MainActivity.java
@@ -10,16 +10,22 @@ package peegee.fullorganizer;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
@@ -70,26 +76,32 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.inject(this);
 
         setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.rotate_logout);
         toolbar.setTitleTextColor(Color.WHITE);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(v.getContext())
+                        .setTitle("Log Out")
+                        .setMessage("Are you sure you want to log out?")
+                        .setIcon(R.drawable.logout)
+                        .setPositiveButton("Log Out", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                AuthUI.getInstance()
+                                        .signOut(getApplicationContext())
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                authenticate();
+                                            }
+                                        });
+                            }})
+                        .setNegativeButton("Cancel", null).show();
+            }
+        });
 
         // Authentication
         firebaseAuth = FirebaseAuth.getInstance();
         authenticate();
-
-        // Database
-        synchronized (DBLOCK) {
-            db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "organizer_database")
-                    .allowMainThreadQueries()
-                    .fallbackToDestructiveMigration()
-                    .build();
-        }
-
-        // Firebase
-        synchronized (FBLOCK) {
-            firebaseDatabase = FirebaseDatabase.getInstance();
-            DatabaseReference databaseReference = firebaseDatabase.getReference();
-//            databaseReference.setValue(db); TODO
-        }
     }
 
     private void authenticate() {
@@ -121,20 +133,24 @@ public class MainActivity extends AppCompatActivity {
                 firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
                 loadDBAccordingToUser();
             } else {
-                // Sign in failed, check response for error code
-                // ...
+                Toast.makeText(getApplication(), "Sign in failed!", Toast.LENGTH_SHORT).show();
+                authenticate();
             }
         }
     }
 
     private void loadDBAccordingToUser() {
-        if (firebaseUser == null) // User not signed in
-        {
+        // Database
+        synchronized (DBLOCK) {
+            db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "organizer_database")
+                    .allowMainThreadQueries()
+                    .fallbackToDestructiveMigration()
+                    .build();
+            // Firebase
+            synchronized (FBLOCK) {
+                // TODO
 
-        }
-        else // User signed in
-        {
-
+            }
         }
 
     }
