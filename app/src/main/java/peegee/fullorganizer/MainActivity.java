a46603f457f39c9c5fe212dba145109fae01327f
PeeGee92 <pierre_ghaly@yahoo.com>
2018-04-21 06:09:40 +0300

    Firebase linked with Room, not working yet

--- app/src/main/java/peegee/fullorganizer/MainActivity.java
+++ app/src/main/java/peegee/fullorganizer/MainActivity.java
@@ -8,7 +8,6 @@
 
 package peegee.fullorganizer;
 
-import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -18,6 +17,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -28,6 +28,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -70,6 +71,11 @@ public class MainActivity extends AppCompatActivity {
     public static final Object FBLOCK = new Object(); // Firebase Lock
     private static FirebaseAuth firebaseAuth;
     private static FirebaseUser firebaseUser;
+    public static DatabaseReference rootRef;
+    public static DatabaseReference userReference;
+    public static DatabaseReference dbReference;
+    public boolean newUser;
+    public static final String DB_PRIMARY_KEY = "user_id";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,10 +89,11 @@ public class MainActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d("SIGNOUT", "onClick: IN!");
                 new AlertDialog.Builder(v.getContext())
                         .setTitle("Log Out")
                         .setMessage("Are you sure you want to log out?")
-                        .setIcon(R.drawable.logout)
+                        .setIcon(R.drawable.rotate_logout)
                         .setPositiveButton("Log Out", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int whichButton) {
                                 AuthUI.getInstance()
@@ -105,6 +112,106 @@ public class MainActivity extends AppCompatActivity {
         if (firebaseUser == null) {
             firebaseAuth = FirebaseAuth.getInstance();
             authenticate();
+        }
+
+        // Firebase Initialization
+        initFirebase();
+
+    }
+
+    // FireBase
+    private void initFirebase() {
+        synchronized (FBLOCK) {
+            // TODO Check if user exists
+            rootRef = FirebaseDatabase.getInstance().getReference();
+            rootRef.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for(DataSnapshot val : dataSnapshot.getChildren()){
+                        if(val.child(DB_PRIMARY_KEY).getValue(String.class).contains(firebaseUser.getUid())) {
+                            newUser = false;
+                            // Database
+                            synchronized (DBLOCK) {
+                                db = val.child(DB_PRIMARY_KEY).child("database").getValue(AppDatabase.class);
+                            }
+                            break;
+                        }
+                        if(newUser != false){
+                            newUser = true;
+                        }
+                    }
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+                    // Do Nothing
+                }
+            });
+
+            if(newUser) {
+                // Database
+                synchronized (DBLOCK) {
+                    db = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "organizer_database")
+                            .allowMainThreadQueries()
+                            .build();
+                }
+                userReference = rootRef.child(DB_PRIMARY_KEY);
+                dbReference = userReference.child("database");
+
+                rootRef.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        userReference.setValue(firebaseUser.getUid());
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        // Do Nothing
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        // Do Nothing
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                        // Do Nothing
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Do Nothing
+                    }
+                });
+
+                userReference.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        dbReference.setValue(db);
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        // Do Nothing
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        // Do Nothing
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                        // Do Nothing
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                        // Do Nothing
+                    }
+                });
+            }
         }
     }
 
@@ -128,6 +235,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        boolean newUser = false; // TODO
 
         if (requestCode == RC_SIGN_IN) {
             IdpResponse response = IdpResponse.fromResultIntent(data);
@@ -153,9 +262,9 @@ public class MainActivity extends AppCompatActivity {
 //            synchronized (FBLOCK) {
 //                firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 //                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
-//                DatabaseReference databaseReference = firebaseDatabase.getReference();
+//                DatabaseReference rootRef = firebaseDatabase.getReference();
 //
-//                databaseReference.addValueEventListener(new ValueEventListener() {
+//                rootRef.addValueEventListener(new ValueEventListener() {
 //                    @Override
 //                    public void onDataChange(DataSnapshot dataSnapshot) {
 //                        // TODO
@@ -167,7 +276,7 @@ public class MainActivity extends AppCompatActivity {
 //                    }
 //                });
 //
-//                databaseReference.setValue(db);
+//                rootRef.setValue(db);
 //
 //            }
         }
@@ -196,15 +305,54 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private static class MyFireBase {
-
-        // Entity references
-        static DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
-        static DatabaseReference notesReference = databaseReference.child("Notes");
-        static DatabaseReference todoReference = databaseReference.child("Todo");
-        static DatabaseReference alarmReference = databaseReference.child("Alarm");
-        static DatabaseReference ReminderReference = databaseReference.child("Reminder");
-
-        // TODO add value event listener
-    }
+//    private static class MyFireBase {
+//
+//        // Entity references
+//        static DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+//        static DatabaseReference notesReference = rootRef.child("Notes");
+//        static DatabaseReference todoReference = rootRef.child("Todo");
+//        static DatabaseReference alarmReference = rootRef.child("Alarm");
+//        static DatabaseReference ReminderReference = rootRef.child("Reminder");
+//
+//        // TEST
+//        static DatabaseReference dbReference = rootRef.child("Database");
+//
+//
+//        // TODO add value event listener
+//        public void test() {
+//
+//            dbReference.child("user_id").addChildEventListener(new ChildEventListener() {
+//                @Override
+//                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+//                    dbReference.child("user_id").setValue(firebaseUser.getUid());
+//                }
+//
+//                @Override
+//                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+//
+//                }
+//
+//                @Override
+//                public void onChildRemoved(DataSnapshot dataSnapshot) {
+//
+//                }
+//
+//                @Override
+//                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+//
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//
+//                }
+//            })
+//
+//            dbReference.setValue(db);
+//            dbReference.child("database_id").setValue(100);
+//            Query query = dbReference.orderByChild("database_id")
+//                    .equalTo(100);
+//
+//        }
+//    }
 }
